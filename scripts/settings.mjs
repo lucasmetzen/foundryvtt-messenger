c0fbbe067549edbcb9d13dab710ae9ed6c58ab14
Lucas Metzen <45624015+lucasmetzenbd@users.noreply.github.com>
2024-12-19 22:09:56 +0100

    Improve code indentation & update .gitignore

--- scripts/settings.mjs
+++ scripts/settings.mjs
@@ -2,68 +2,68 @@ import { MODULE_ID } from "./config.mjs";
 import { log } from "./helpers/log.mjs";
 
 export function registerSettings() {
-  registerSetting('showNotificationForNewWhisper', {
-    name: "LAME.Setting.ShowNotificationForNewWhisper",
-    hint: "LAME.Setting.ShowNotificationForNewWhisperHint",
-    scope: 'client',
-    config: true,
-    type: Boolean,
-    default: false,
-    requiresReload: false
-  });
+	registerSetting('showNotificationForNewWhisper', {
+		name: "LAME.Setting.ShowNotificationForNewWhisper",
+		hint: "LAME.Setting.ShowNotificationForNewWhisperHint",
+		scope: 'client',
+		config: true,
+		type: Boolean,
+		default: false,
+		requiresReload: false
+	});
 
-  registerSetting('permanentNotificationForNewWhisper', {
-    name: "LAME.Setting.PermanentNotificationForNewWhisper",
-    hint: "LAME.Setting.PermanentNotificationForNewWhisperHint",
-    scope: 'client',
-    config: true,
-    type: Boolean,
-    default: false,
-    requiresReload: false
-  });
+	registerSetting('permanentNotificationForNewWhisper', {
+		name: "LAME.Setting.PermanentNotificationForNewWhisper",
+		hint: "LAME.Setting.PermanentNotificationForNewWhisperHint",
+		scope: 'client',
+		config: true,
+		type: Boolean,
+		default: false,
+		requiresReload: false
+	});
 
-  registerSetting('buttonInSceneControlToolbar', {
-    name: "LAME.Setting.ButtonInSceneControlToolbar",
-    hint: "LAME.Setting.ButtonInSceneControlToolbarHint",
-    scope: 'client',
-    config: true,
-    type: Boolean,
-    default: false,
-    requiresReload: false,
-    onChange: () => {
-      window.ui.controls.render();
-    }
-  });
+	registerSetting('buttonInSceneControlToolbar', {
+		name: "LAME.Setting.ButtonInSceneControlToolbar",
+		hint: "LAME.Setting.ButtonInSceneControlToolbarHint",
+		scope: 'client',
+		config: true,
+		type: Boolean,
+		default: false,
+		requiresReload: false,
+		onChange: () => {
+			window.ui.controls.render();
+		}
+	});
 
-  registerSetting('buttonInChatControls', {
-    name: "LAME.Setting.ButtonInChatControl",
-    hint: "LAME.Setting.ButtonInChatControlHint",
-    scope: 'client',
-    config: true,
-    type: Boolean,
-    default: true,
-    requiresReload: true
-  });
+	registerSetting('buttonInChatControls', {
+		name: "LAME.Setting.ButtonInChatControl",
+		hint: "LAME.Setting.ButtonInChatControlHint",
+		scope: 'client',
+		config: true,
+		type: Boolean,
+		default: true,
+		requiresReload: true
+	});
 
-  registerSetting('showInactiveUsers', {
-    name: "LAME.Setting.ShowInactiveUsers",
-    hint: "LAME.Setting.ShowInactiveUsersHint",
-    scope: 'client',
-    config: true,
-    type: Boolean,
-    default: false,
-    onChange: async() => {
-      await window.LAME.computeUsersDataAndRenderPartial();
-    }
-  });
+	registerSetting('showInactiveUsers', {
+		name: "LAME.Setting.ShowInactiveUsers",
+		hint: "LAME.Setting.ShowInactiveUsersHint",
+		scope: 'client',
+		config: true,
+		type: Boolean,
+		default: false,
+		onChange: async() => {
+			await window.LAME.computeUsersDataAndRenderPartial();
+		}
+	});
 
-  log("Module settings registered."); // DEBUG: Remove after development.
+	log("Module settings registered."); // DEBUG: Remove after development.
 }
 
 function registerSetting(settingName, options) {
-  return game.settings.register(MODULE_ID, settingName, options);
+	return game.settings.register(MODULE_ID, settingName, options);
 }
 
 export function getSetting(settingName) {
-  return game.settings.get(MODULE_ID, settingName);
+	return game.settings.get(MODULE_ID, settingName);
 }
