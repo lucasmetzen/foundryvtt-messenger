1324b811f49f998b858561c9d8ace59a57597980
Lucas Metzen <45624015+lucasmetzen@users.noreply.github.com>
2025-01-07 21:24:34 +0100

    💄Change module icon to outlined one
    This avoids confusion as it's identical to core's "chat" sidebar tab
    icon. The outlined one is chosen as it indicated the conversation being
    quieter than a non-whispered one.

--- scripts/config.mjs
+++ scripts/config.mjs
@@ -1,5 +1,5 @@
 export const MODULE_ID = "lame-messenger",
-	MODULE_ICON_CLASSES = "fas fa-comments",
+	MODULE_ICON_CLASSES = "far fa-comments",
 	CONSOLE_LOG_PREFIX = "LAME Messenger";
 
 const PATH = `modules/${MODULE_ID}`,
